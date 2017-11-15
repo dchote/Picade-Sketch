9d8ba02333cb3eb8e16448211cc679ae1f497b64
Daniel Chote <daniel@chote.com>
2017-11-15 09:32:19 -0500

    tweaking analog saturation

--- Picade/Picade.ino
+++ Picade/Picade.ino
@@ -159,7 +159,7 @@ void update(void) {
       // carcade HAX for smaller range on pots
       int hax_state;
       
-      hax_state = map(analogRead(picade_pins[i]), 0, 1023, -50, 304);
+      hax_state = map(analogRead(picade_pins[i]), 0, 1023, -75, 329);
       
       if (hax_state < 0) 
       {
